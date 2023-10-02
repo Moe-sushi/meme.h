9b7b740446d00e548befee2bfa790d147a994f58
渢鸞 <moe-hacker@outlook.com>
2023-10-02 15:21:13 +0800

    Update meme.h

--- meme.h
+++ meme.h
@@ -32,13 +32,17 @@
 // 虽然false应该是((_Bool)+0u)，但!true显然更抽象。
 #define true ((_Bool)+1u)
 #define false !true
-// 众所周知一坤=2.5
+// 众所周知一坤为2.5
 #define kun ((float)2.5F)
 // 一花西币为79CNY
-#define huaxi ((int)79)
+// 花西子显然不算国货，因此这里混个罗马音同时保证了抽象性
+#define huashi ((int)79)
 // 众所周知1141514和1919810是两个经典大幻数
 #define number ((int)114514)
 #define bignumber ((int)1919810)
+// An evil.
+#define lowTowards -->
+#define highTowards ++<
 // 不需要多解释
 #define 寄 exit(1)
 #define 淦(发生肾磨石了) printf("\033[31m握草，%s\n\033[0m",发生肾磨石了);
