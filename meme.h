a88d3948a4c6d9e3de2a00426eb97baa91238f30
渢鸞 <moe-hacker@outlook.com>
2023-10-02 15:59:39 +0800

    Update meme.h

--- meme.h
+++ meme.h
@@ -1,32 +1,27 @@
-// SPDX-License-Identifier: MIT
-/*
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
  *
- * This file is part of meme.h, with ABSOLUTELY NO WARRANTY.
- *
- * MIT License
- *
- * Copyright (c) 2023 Moe-hacker
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *
- */
+**/
+// 仅供娱乐，切勿在生产中使用此文件
 #include <stdio.h>
 #include <stdlib.h>
 // 虽然false应该是((_Bool)+0u)，但!true显然更抽象。
@@ -46,3 +41,15 @@
 // 不需要多解释
 #define 寄 exit(1)
 #define 淦(发生肾磨石了) printf("\033[31m握草，%s\n\033[0m",发生肾磨石了);
+// 中文编程，遥遥领先！
+typedef char 字符型;
+typedef int 整型;
+typedef long 长整型;
+typedef float 浮点型;
+typedef double 双精度浮点型;
+// 虽然等于什么都没有，但是加上这三个代码会更抽象
+#define 接
+#define 化
+#define 发
+// 敏感词规避
+#define SIXTY-FOUR (0b1000000)
